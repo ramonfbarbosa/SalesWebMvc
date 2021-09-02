07a3636ed44a69f56a1c9e5d9aa3725a93af3eed
Ramon Barbosa <ramonmfb777@gmail.com>
2021-09-02 14:15:41 -0300

    CRUD Scaffolding

---
+++ webmvc/Data/webmvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace webmvc.Models
+{
+    public class webmvcContext : DbContext
+    {
+        public webmvcContext (DbContextOptions<webmvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<webmvc.Models.Department> Department { get; set; }
+    }
+}
