cc481fbda08e6c6b25ff4791488399a67d4f8dc7
Ramon Barbosa <ramonmfb777@gmail.com>
2021-09-02 15:32:54 -0300

    MySQL adaptation and first migration

--- webmvc/Startup.cs
+++ webmvc/Startup.cs
@@ -37,7 +37,8 @@ namespace webmvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<webmvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("webmvcContext")));
+                    options.UseMySql(Configuration.GetConnectionString("webmvcContext"), builder =>
+                        builder.MigrationsAssembly("webmvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
