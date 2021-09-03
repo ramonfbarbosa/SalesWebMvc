8a60f18ad050a57a231ec2e91d7993dfeb794fa5
Ramon Barbosa <ramonmfb777@gmail.com>
2021-09-03 15:25:25 -0300

    Other entities and second migration

--- webmvc/Migrations/20210903182243_OtherEntities.Designer.cs
+++ webmvc/Migrations/20210903182243_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using webmvc.Models;
 
 namespace webmvc.Migrations
 {
     [DbContext(typeof(webmvcContext))]
-    partial class webmvcContextModelSnapshot : ModelSnapshot
+    [Migration("20210903182243_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- webmvc/Models/Department.cs
+++ webmvc/Models/Department.cs
@@ -1,10 +1,32 @@
-﻿namespace webmvc.Models
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+namespace webmvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
-       
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSeller(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(Seller => Seller.TotalSales(initial, final));
+        }
     }
 }
